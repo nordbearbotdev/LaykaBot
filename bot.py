c4daf38aa0bd707ac61fa5fca456c3b1229c28d5
Commit 404 <85753549+nordbearbotdev@users.noreply.github.com>
2022-06-28 18:39:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,13 @@
+import disocrd
+import random
+from discord.ext import commands
 
+PREFIX = ''
+
+clinet = commands.Bot( command_prefix = PREFIX )
+client.remove_command( 'help' )
+
+@client.event
+
+async def on_ready():
+  print('Под поключен✅')
