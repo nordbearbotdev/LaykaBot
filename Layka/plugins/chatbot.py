79d78420f7292955c0860cc36569ef5de8236947
Commit 404 <85753549+nordbearbotdev@users.noreply.github.com>
2022-07-06 23:30:19 +0300

    Delete Layka/plugins directory

--- Layka/plugins/chatbot.py
+++
@@ -1,9 +0,0 @@
-import disnake
-from aiohttp import request
-from disnake.ext import commands
-import database as db
-import constants as var
-from functions import get_prefix
-from ext.permissions import has_command_permission
-
-
